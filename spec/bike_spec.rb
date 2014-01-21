966bd1877434f0ebd9f38d1a5a65f9911fa0592b
biwek <biweks@gmail.com>
2014-01-21 13:52:51 +0000

    refactored bike class and its test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,24 +2,23 @@ require_relative '../lib/bike'
 
 describe "Bike" do 
 
+	let(:bike) {Bike.new}
+
 	it "should not be broken after its created" do 
-		bike = Bike.new
 		# a method broken that should return false
 		expect(bike).not_to be_broken
 		# same as=> expect(bike.broken?).to be_false
 	end
 
 	it "should be able to break" do
-		bike = Bike.new
 		bike.break
 		expect(bike).to be_broken
 	end
 
 	it "should be able to fix broken bike" do 
-		bike = Bike.new 
 		bike.break 
 		bike.fix
 		expect(bike).not_to be_broken
 	end
-	
+
 end
