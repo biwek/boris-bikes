de4fd7e6a556b3e24ad1cb615b951b4cf6ba7a5c
biwek <biweks@gmail.com>
2014-01-21 14:28:18 +0000

    added limit for the number of bikes that can be docked

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,10 @@
 class DockingStation
 
-	def initialize
-		@bikes = []
+	DEFAULT_CAPACITY = 10
 
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+		@bikes = [] 
 	end
 
 	def bike_count
@@ -15,6 +17,10 @@ class DockingStation
 
 	def release(bike)
 		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
 	end
 end
 
